459edf8f0b7dbb86919b3ab0eec6b54e7f3023c5
Adrian <adrianmouzinhopro@gmail.com>
2021-05-18 11:04:17 -0300

    Buttons next and previous

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -15,7 +15,9 @@ export function Player() {
         currentEpisodeIndex,
         isPlaying,
         setPlayingState,
-        togglePlay
+        togglePlay,
+        playNext,
+        playPrevious
     } = useContext(PlayerContext)
 
     useEffect(() => {
@@ -87,7 +89,7 @@ export function Player() {
                     <button type="button" disabled={!episode}>
                         <img src="/shuffle.svg" alt="Embaralhar" />
                     </button>
-                    <button type="button" disabled={!episode}>
+                    <button type="button" onClick={playPrevious} disabled={!episode}>
                         <img src="/play-previous.svg" alt="Tocar anterior" />
                     </button>
                     <button
@@ -100,7 +102,7 @@ export function Player() {
                             ? <img src="/pause.svg" alt="Tocar" />
                             : <img src="/play.svg" alt="Tocar" />}
                     </button>
-                    <button type="button" disabled={!episode}>
+                    <button type="button" onClick={playNext} disabled={!episode}>
                         <img src="/play-next.svg" alt="Tocar próximo" />
                     </button>
                     <button type="button" disabled={!episode}>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,7 +31,9 @@ type HomeProps = {
 }
 
 export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
-  const { play } = useContext(PlayerContext)
+  const { playList } = useContext(PlayerContext)
+
+  const episodeList = [...latestEpisodes, ...allEpisodes];
 
   return (
     <div className={styles.homepage}>
@@ -39,7 +41,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
         <h2>Últimos lançamentos</h2>
 
         <ul>
-          {latestEpisodes.map(episode => {
+          {latestEpisodes.map((episode, index) => {
             return (
               <li key={episode.id}>
                 <Image
@@ -59,7 +61,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                   <span>{episode.durationAsString}</span>
                 </div>
 
-                <button type="button" onClick={() => play(episode)}>
+                <button type="button" onClick={() => playList(episodeList, index)}>
                   <img src="/play-green.svg" alt="Tocar episódio" />
                 </button>
               </li>
@@ -83,7 +85,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
             </tr>
           </thead>
           <tbody>
-            {allEpisodes.map(episode => {
+            {allEpisodes.map((episode, index) => {
               return (
                 <tr key={episode.id}>
                   <td style={{ width: 60 }}>
@@ -104,7 +106,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                   <td style={{ width: 80 }}>{episode.publishedAt}</td>
                   <td>{episode.durationAsString}</td>
                   <td>
-                    <button type="button" onClick={() => play(episode)}>
+                    <button type="button" onClick={() => playList(episodeList, index + latestEpisodes.length)}>
                       <img src="/play-green.svg" alt="Tocar episódio" />
                     </button>
                   </td>
